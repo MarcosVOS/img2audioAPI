781432a539896df6c1e57d13e17f54f7ea2e7fe4
MarcosVOS <marcos.oliveiradev@gmail.com>
2025-05-31 21:07:11 -0300

    feat: add /text2audio endpoint that generates and returns TTS audio using OpenAI

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from fastapi import Depends, FastAPI
 from fastapi import FastAPI, File, UploadFile
 from typing_extensions import Annotated
 import config
+from pathlib import Path
+from fastapi.responses import FileResponse
+
 from openai import OpenAI
-import base64
 from app.api.v1.endpoints import image_text
 
 app = FastAPI(
@@ -20,5 +22,24 @@ def get_settings():
 @app.get("/status")
 async def status():
     return {"status": "ok"}
+
+@app.get("/text2audio")
+async def text_to_audio(settings: Annotated[config.Settings, Depends(get_settings)]):
+    client = OpenAI(api_key=settings.openai_api_key)
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+
+    with client.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        voice="coral",
+        input="Olá, este é um teste da conversão de texto para áudio.",
+        instructions="Fale em um tom alegre e positivo."
+    ) as response: 
+        response.stream_to_file(speech_file_path)
+
+    return FileResponse(
+        path=speech_file_path,
+        media_type="audio/mpeg",
+        filename="speech.mp3"
+    )
 
 app.include_router(image_text.router, prefix="/api/v1", tags=["Image to Text"])
